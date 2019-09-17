e38f4e2148c4a5817bd124929761c0fc774732f3
sahilsaini1757 <sahilsaini1757@gmail.com>
2019-09-17 10:24:24 +0530

    new commit

---
+++ db/migrate/20190908122639_create_subjects.rb
@@ -0,0 +1,11 @@
+class CreateSubjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :subjects do |t|
+      t.string :title
+      t.string :content
+      t.boolean :visible
+
+      t.timestamps
+    end
+  end
+end
